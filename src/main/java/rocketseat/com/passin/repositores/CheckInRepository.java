59425e57f1870dbc95ad07a32e778b0a3bc37832
Rian Lima <rianlima235.rl@gmail.com>
2024-04-04 17:16:14 -0400

    Adicionado o tratamento para excecões

--- src/main/java/rocketseat/com/passin/repositores/CheckInRepository.java
+++ src/main/java/rocketseat/com/passin/repositores/CheckInRepository.java
@@ -3,5 +3,8 @@ package rocketseat.com.passin.repositores;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rocketseat.com.passin.domain.checkin.CheckIn;
 
+import java.util.Optional;
+
 public interface CheckInRepository extends JpaRepository<CheckIn, Integer> {
+    Optional<CheckIn> findByAttendeeId(String attendeeId);
 }
